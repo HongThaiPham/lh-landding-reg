dda7bc4342a99a1c02cce4e421dd7b8e431d4757
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2020-04-06 15:37:31 +0700

    sua theo file tuyen sinh

--- assets/js/leo-v-reg.js
+++ assets/js/leo-v-reg.js
@@ -1,27 +1,28 @@
 const headerHttp = {
   headers: {
     Accept: "application/json",
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 };
 
 const messages = {
-  _default: field => `${field} không hợp lệ.`,
+  _default: (field) => `${field} không hợp lệ.`,
   after: (field, [target, inclusion]) =>
     `The ${field} must be after ${inclusion ? "or equal to " : ""}${target}.`,
-  alpha: field => `The ${field} field may only contain alphabetic characters.`,
-  alpha_dash: field =>
+  alpha: (field) =>
+    `The ${field} field may only contain alphabetic characters.`,
+  alpha_dash: (field) =>
     `The ${field} field may contain alpha-numeric characters as well as dashes and underscores.`,
-  alpha_num: field =>
+  alpha_num: (field) =>
     `The ${field} field may only contain alpha-numeric characters.`,
-  alpha_spaces: field =>
+  alpha_spaces: (field) =>
     `The ${field} field may only contain alphabetic characters as well as spaces.`,
   before: (field, [target, inclusion]) =>
     `The ${field} must be before ${inclusion ? "or equal to " : ""}${target}.`,
   between: (field, [min, max]) =>
     `The ${field} field must be between ${min} and ${max}.`,
-  confirmed: field => `The ${field} confirmation does not match.`,
-  credit_card: field => `The ${field} field is invalid.`,
+  confirmed: (field) => `The ${field} confirmation does not match.`,
+  credit_card: (field) => `The ${field} field is invalid.`,
   date_between: (field, [min, max]) =>
     `The ${field} must be between ${min} and ${max}.`,
   date_format: (field, [format]) => `${field} phải có định dạng ${format}.`,
@@ -33,14 +34,15 @@ const messages = {
     `The ${field} field must be numeric and exactly contain ${length} digits.`,
   dimensions: (field, [width, height]) =>
     `The ${field} field must be ${width} pixels by ${height} pixels.`,
-  email: field => `${field} phải là email hợp lệ.`,
-  excluded: field => `The ${field} field must be a valid value.`,
-  ext: field => `The ${field} field must be a valid file.`,
-  image: field => `The ${field} field must be an image.`,
-  included: field => `The ${field} field must be a valid value.`,
-  integer: field => `The ${field} field must be an integer.`,
-  ip: field => `The ${field} field must be a valid ip address.`,
-  ip_or_fqdn: field => `The ${field} field must be a valid ip address or FQDN.`,
+  email: (field) => `${field} phải là email hợp lệ.`,
+  excluded: (field) => `The ${field} field must be a valid value.`,
+  ext: (field) => `The ${field} field must be a valid file.`,
+  image: (field) => `The ${field} field must be an image.`,
+  included: (field) => `The ${field} field must be a valid value.`,
+  integer: (field) => `The ${field} field must be an integer.`,
+  ip: (field) => `The ${field} field must be a valid ip address.`,
+  ip_or_fqdn: (field) =>
+    `The ${field} field must be a valid ip address or FQDN.`,
   length: (field, [length, max]) => {
     if (max) {
       return `Độ dài của ${field} phải giữa ${length} và ${max}.`;
@@ -51,25 +53,25 @@ const messages = {
   max: (field, [length]) => `${field} không thể dài hơn ${length} ký tự.`,
   max_value: (field, [max]) =>
     `Giá trị của ${field} phải nhỏ hơn hoặc bằng ${max}.`,
-  mimes: field => `The ${field} field must have a valid file type.`,
+  mimes: (field) => `The ${field} field must have a valid file type.`,
   min: (field, [length]) => `${field} phải dài hơn ${length} ký tự.`,
   min_value: (field, [min]) =>
     `Giá trị của ${field} phải lớn hơn hoặc bằng ${min}.`,
-  numeric: field => `The ${field} field may only contain numeric characters.`,
-  regex: field => `The ${field} field format is invalid.`,
-  required: field => `${field} không thể bỏ trống`,
+  numeric: (field) => `The ${field} field may only contain numeric characters.`,
+  regex: (field) => `The ${field} field format is invalid.`,
+  required: (field) => `${field} không thể bỏ trống`,
   required_if: (field, [target]) =>
     `The ${field} field is required when the ${target} field has this value.`,
   size: (field, [size]) =>
     `The ${field} size must be less than ${formatFileSize(size)}.`,
-  url: field => `The ${field} field is not a valid URL.`
+  url: (field) => `The ${field} field is not a valid URL.`,
 };
 
 Vue.use(VeeValidate, {
   locale: "vi",
   dictionary: {
-    vi: { attributes: {}, messages }
-  }
+    vi: { attributes: {}, messages },
+  },
 });
 
 var app = new Vue({
@@ -93,14 +95,14 @@ var app = new Vue({
       lsDoiTuong: [],
       lsToHop: [],
       lsMon: [],
-
+      lsDTTT: [],
       lsPhuongThuc: [
-        { value: 0, name: "Tổ hợp 3 môn" },
+        { value: 5, name: "Điểm trung bình 3 học kỳ" },
+        { value: 0, name: "Tổ hợp 3 môn lớp 12" },
         { value: 1, name: "Điểm TB cả năm 12" },
         { value: 2, name: "Thi THPT quốc gia" },
         { value: 3, name: "Đánh giá năng lực" },
         { value: 4, name: "Tuyển thẳng" },
-        { value: 5, name: "Điểm trung bình 3 học kỳ" }
       ],
       maNganh: null,
       dataForm: {
@@ -131,7 +133,8 @@ var app = new Vue({
         PhuongThucXetTuyen: 0,
         CMND: "",
         SoBD: "",
-        TuyenThang: ""
+        TuyenThang: "",
+        DoiTuongTuyenThang: null,
       },
       isDiemTB: false,
       isDGNL: false,
@@ -140,7 +143,7 @@ var app = new Vue({
       src: "default",
       ctvid: "",
       ctv: null,
-      idDangKy: ""
+      idDangKy: "",
     };
   },
   mounted() {
@@ -165,8 +168,8 @@ var app = new Vue({
     this.loading = false;
   },
   watch: {
-    "dataForm.PhuongThucXetTuyen": function(val) {
-      if (val === 0 || val === 2 || val === 5) {
+    "dataForm.PhuongThucXetTuyen": function (val) {
+      if (val === 0 || val === 2 || val === 5 || val === 4) {
         this.isDiemTB = false;
       } else {
         this.isDiemTB = true;
@@ -176,41 +179,56 @@ var app = new Vue({
       } else {
         this.isDGNL = false;
       }
+      if (val === 4) {
+        this.getDoiTuongTuyenThang();
+      }
     },
-    "dataForm.MaTinhTPLop10": function(val) {
+    "dataForm.MaTinhTPLop10": function (val) {
       this.getQuanHuyen(10);
     },
-    "dataForm.MaQuanHuyenLop10": function(val) {
+    "dataForm.MaQuanHuyenLop10": function (val) {
       this.getTruong(10);
     },
-    "dataForm.MaTinhTPLop11": function(val) {
+    "dataForm.MaTinhTPLop11": function (val) {
       this.getQuanHuyen(11);
     },
-    "dataForm.MaQuanHuyenLop11": function(val) {
+    "dataForm.MaQuanHuyenLop11": function (val) {
       this.getTruong(11);
     },
-    "dataForm.MaTinhTPLop12": function(val) {
+    "dataForm.MaTinhTPLop12": function (val) {
       this.getQuanHuyen(12);
     },
-    "dataForm.MaQuanHuyenLop12": function(val) {
+    "dataForm.MaQuanHuyenLop12": function (val) {
       this.getTruong(12);
     },
-    "dataForm.NganhID": function(val) {
+    "dataForm.NganhID": function (val) {
       this.handleChangeNganh();
     },
-    "dataForm.ToHopMonID": function(val) {
+    "dataForm.ToHopMonID": function (val) {
       this.handleChangeToHop(val);
-    }
+    },
   },
   methods: {
+    getDoiTuongTuyenThang() {
+      fetch(`${this.tapiUrl}/DoiTuongTuyenThang`, {
+        method: "GET",
+        ...headerHttp,
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if (res.data) {
+            this.lsDTTT = res.data;
+          }
+        });
+    },
     getInfoById() {
       fetch(`${this.tapiUrl}/DangKyOnline_byId`, {
         method: "POST",
         ...headerHttp,
-        body: JSON.stringify({ MaDangKy: this.idDangKy })
+        body: JSON.stringify({ MaDangKy: this.idDangKy }),
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
           if (res.data && res.data[0]) {
             this.dataForm = { ...res.data[0] };
           }
@@ -221,10 +239,10 @@ var app = new Vue({
       if (this.ctvid) {
         fetch(`${this.tapiUrl}/obj/ctv_get/${this.ctvid}`, {
           method: "GET",
-          ...headerHttp
+          ...headerHttp,
         })
-          .then(res => res.json())
-          .then(res => {
+          .then((res) => res.json())
+          .then((res) => {
             console.log(res);
             if (res && res.data.HoTen && res.data.DienThoai) {
               this.ctv = res.data;
@@ -267,35 +285,36 @@ var app = new Vue({
         PhuongThucXetTuyen: 0,
         CMND: "",
         SoBD: "",
-        TuyenThang: ""
+        TuyenThang: "",
+        DoiTuongTuyenThang: null,
       };
     },
     getNganh() {
       fetch(`${this.tapiUrl}/nganh_select/0`, {
         method: "GET",
-        ...headerHttp
+        ...headerHttp,
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
           this.lsNganh = res.data;
         });
     },
     getTinhThanh() {
       fetch(`${this.tapiUrl}/TinhTP`, {
         method: "GET",
-        ...headerHttp
+        ...headerHttp,
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
           this.lsTinhThanh = res.data;
         });
 
       fetch(`${this.tapiUrl}/tinhthanh_select`, {
         method: "GET",
-        ...headerHttp
+        ...headerHttp,
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
           this.lsTinhThanhSinh = res.data;
         });
     },
@@ -306,10 +325,10 @@ var app = new Vue({
       fetch(`${this.tapiUrl}/QuanHuyen_Select`, {
         method: "POST",
         body: JSON.stringify({ MaTinhTP }),
-        ...headerHttp
+        ...headerHttp,
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
           this[`lsQuanHuyen${control}`] = res.data;
         });
     },
@@ -320,36 +339,36 @@ var app = new Vue({
       fetch(`${this.tapiUrl}/TruongTHPT`, {
         method: "POST",
         body: JSON.stringify({ MaQuanHuyen, MaTinhTP }),
-        ...headerHttp
+        ...headerHttp,
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
           this[`lsTruongTHPT${control}`] = res.data;
         });
     },
     getKhuVuc() {
       fetch(`${this.tapiUrl}/khuvuc_select/0`, {
         method: "GET",
-        ...headerHttp
+        ...headerHttp,
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
           this.lsKhuVuc = res.data;
         });
     },
     getDoiTuong() {
       fetch(`${this.tapiUrl}/doituong_select/0`, {
         method: "GET",
-        ...headerHttp
+        ...headerHttp,
       })
-        .then(res => res.json())
-        .then(res => {
+        .then((res) => res.json())
+        .then((res) => {
           this.lsDoiTuong = res.data;
         });
     },
     processForm() {
       console.log(this.dataForm);
-      this.$validator.validateAll().then(result => {
+      this.$validator.validateAll().then((result) => {
         if (result) {
           // eslint-disable-next-line
           // alert("Form Submitted!");
@@ -382,25 +401,31 @@ var app = new Vue({
                   : 0,
                 ToHopMonID: this.dataForm.ToHopMonID || 0,
                 source: this.src || "default",
-                ctvid: this.ctvid || ""
+                ctvid: this.ctvid || "",
+                MaTinhTPLop10: this.dataForm.MaTinhTPLop12,
+                MaQuanHuyenLop10: this.dataForm.MaQuanHuyenLop12,
+                MaTruongLop10: this.dataForm.MaTruongLop12,
+                MaTinhTPLop11: this.dataForm.MaTinhTPLop12,
+                MaQuanHuyenLop11: this.dataForm.MaQuanHuyenLop12,
+                MaTruongLop11: this.dataForm.MaTruongLop12,
               };
               return fetch(`${this.tapiUrl}/DangKyOnline_Update`, {
                 method: "POST",
                 ...headerHttp,
-                body: JSON.stringify(data)
+                body: JSON.stringify(data),
               })
-                .then(res => {
+                .then((res) => {
                   if (!res.ok) {
                     throw new Error(res.statusText);
                   }
                   return res.json();
                 })
-                .catch(error => {
+                .catch((error) => {
                   Swal.showValidationMessage(`Request failed: ${error}`);
                 });
             },
-            allowOutsideClick: false
-          }).then(result => {
+            allowOutsideClick: false,
+          }).then((result) => {
             if (result.value) {
               this.resetForm();
               console.log(this.dataForm);
@@ -419,27 +444,35 @@ var app = new Vue({
           title: "Thiếu thông tin",
           text: "Vui lòng nhập đầy đủ thông tin.",
           type: "error",
-          confirmButtonText: "Ok"
+          confirmButtonText: "Ok",
         });
       });
     },
+    handleChangeTuyenThang() {
+      const mota =
+        this.lsDTTT.find(
+          (x) => x.IdTuyenThang == this.dataForm.DoiTuongTuyenThang
+        ).MoTaTuyenThang || "";
+      this.dataForm.TuyenThang = mota;
+    },
     handleChangeNganh(e) {
-      if (
-        this.lsNganh.filter(x => x.NganhID === this.dataForm.NganhID).length > 0
-      ) {
-        this.maNganh =
-          this.lsNganh.find(x => x.NganhID === this.dataForm.NganhID)
-            .MaNganhMoi || "";
+      // if (
+      //   this.lsNganh.filter((x) => x.NganhID === this.dataForm.NganhID).length >
+      //   0
+      // ) {
+      //   this.maNganh =
+      //     this.lsNganh.find((x) => x.NganhID === this.dataForm.NganhID)
+      //       .MaNganhMoi || "";
 
-        fetch(`${this.tapiUrl}/tohop/${this.dataForm.NganhID}`, {
-          method: "GET",
-          ...headerHttp
-        })
-          .then(res => res.json())
-          .then(res => {
-            this.lsToHop = res.data;
-          });
-      }
+      fetch(`${this.tapiUrl}/tohop/${this.dataForm.NganhID}`, {
+        method: "GET",
+        ...headerHttp,
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          this.lsToHop = res.data;
+        });
+      // }
     },
     handleChangePhuongThuc() {
       this.dataForm.DiemMon1 = null;
@@ -464,10 +497,10 @@ var app = new Vue({
       if (this.dataForm.ToHopMonID) {
         fetch(`${this.tapiUrl}/mon/${this.dataForm.ToHopMonID}`, {
           method: "GET",
-          ...headerHttp
+          ...headerHttp,
         })
-          .then(res => res.json())
-          .then(res => {
+          .then((res) => res.json())
+          .then((res) => {
             this.lsMon = res.data;
           });
       }
@@ -487,6 +520,6 @@ var app = new Vue({
         this.dataForm.MaQuanHuyenLop12 = this.dataForm.MaQuanHuyenLop10;
         this.dataForm.MaTruongLop12 = this.dataForm.MaTruongLop10;
       }
-    }
-  }
+    },
+  },
 });
